64f3ef13069a66f9e592deb6a78fbf6ffb66ab81
Pedro <pierreamaury@outlook.fr>
2019-06-10 23:18:17 +0200

    Add files via upload
    version du 10/06/2019

---
+++ CRUD/functions.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Home
+ * Date: 30/05/2019
+ * Time: 18:30
+ */
+$pdo = new PDO('mysql:dbname=android_motstordus;host=localhost', 'root', '',array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'));
+
+?>
