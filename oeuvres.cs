949d39998409fcaf7eaf20fa0963dfc8a1289a80
Pedro <pierreamaury@outlook.fr>
2019-06-10 23:03:37 +0200

    Add files via upload
    Ajout d'une colonne dans la table question avec des indices.
    Ajout des oeuvres. Il n'y a pas autant d'oeuvres que de questions, car certaines questions traitent de la meme oeuvre.

---
+++ oeuvres.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+namespace Android_motstordus
+{
+    #region Oeuvres
+    public class Oeuvres
+    {
+        #region Member Variables
+        protected int _id_oeuvres;
+        protected string _titre_oeuvres;
+        protected string _auteur_oeuvres;
+        protected string _ref_oeuvres;
+        protected string _codeimage_oeuvres;
+        #endregion
+        #region Constructors
+        public Oeuvres() { }
+        public Oeuvres(string titre_oeuvres, string auteur_oeuvres, string ref_oeuvres, string codeimage_oeuvres)
+        {
+            this._titre_oeuvres=titre_oeuvres;
+            this._auteur_oeuvres=auteur_oeuvres;
+            this._ref_oeuvres=ref_oeuvres;
+            this._codeimage_oeuvres=codeimage_oeuvres;
+        }
+        #endregion
+        #region Public Properties
+        public virtual int Id_oeuvres
+        {
+            get {return _id_oeuvres;}
+            set {_id_oeuvres=value;}
+        }
+        public virtual string Titre_oeuvres
+        {
+            get {return _titre_oeuvres;}
+            set {_titre_oeuvres=value;}
+        }
+        public virtual string Auteur_oeuvres
+        {
+            get {return _auteur_oeuvres;}
+            set {_auteur_oeuvres=value;}
+        }
+        public virtual string Ref_oeuvres
+        {
+            get {return _ref_oeuvres;}
+            set {_ref_oeuvres=value;}
+        }
+        public virtual string Codeimage_oeuvres
+        {
+            get {return _codeimage_oeuvres;}
+            set {_codeimage_oeuvres=value;}
+        }
+        #endregion
+    }
+    #endregion
+}
---
+++ questions.cs
@@ -0,0 +1,77 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+namespace Android_motstordus
+{
+    #region Questions
+    public class Questions
+    {
+        #region Member Variables
+        protected int _id_questions;
+        protected string _libelle_questions;
+        protected string _choix_questions;
+        protected string _choix_questions;
+        protected string _choix_questions;
+        protected string _solution_questions;
+        protected string _libellecategorie_questions;
+        protected string _indice_questions;
+        #endregion
+        #region Constructors
+        public Questions() { }
+        public Questions(string libelle_questions, string choix_questions, string choix_questions, string choix_questions, string solution_questions, string libellecategorie_questions, string indice_questions)
+        {
+            this._libelle_questions=libelle_questions;
+            this._choix_questions=choix_questions;
+            this._choix_questions=choix_questions;
+            this._choix_questions=choix_questions;
+            this._solution_questions=solution_questions;
+            this._libellecategorie_questions=libellecategorie_questions;
+            this._indice_questions=indice_questions;
+        }
+        #endregion
+        #region Public Properties
+        public virtual int Id_questions
+        {
+            get {return _id_questions;}
+            set {_id_questions=value;}
+        }
+        public virtual string Libelle_questions
+        {
+            get {return _libelle_questions;}
+            set {_libelle_questions=value;}
+        }
+        public virtual string Choix_questions
+        {
+            get {return _choix_questions;}
+            set {_choix_questions=value;}
+        }
+        public virtual string Choix_questions
+        {
+            get {return _choix_questions;}
+            set {_choix_questions=value;}
+        }
+        public virtual string Choix_questions
+        {
+            get {return _choix_questions;}
+            set {_choix_questions=value;}
+        }
+        public virtual string Solution_questions
+        {
+            get {return _solution_questions;}
+            set {_solution_questions=value;}
+        }
+        public virtual string Libellecategorie_questions
+        {
+            get {return _libellecategorie_questions;}
+            set {_libellecategorie_questions=value;}
+        }
+        public virtual string Indice_questions
+        {
+            get {return _indice_questions;}
+            set {_indice_questions=value;}
+        }
+        #endregion
+    }
+    #endregion
+}
